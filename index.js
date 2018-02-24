9fc2319bd81226b47475418d106b994950e83a03
Yard <wengyd@huoqiuapp.com>
2018-02-24 15:58:51 +0800

    v

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
-var path = require('path');
-var util = require('util')
-var tstr = '├── '
-var strNow = ''
+const fs = require('fs');
+const path = require('path');
+
+const tstr = '├── ';
+let strNow = '';
 
 function walk(dir) {
-  var files = fs.readdirSync(dir);
-  strNow = tstr + strNow
-  files.forEach(function (e, i) {
-    var pathNow = path.join(dir, e);
-    console.log(strNow + path.relative(dir,pathNow))
-    var stats = fs.statSync(pathNow);
-    if (stats.isDirectory()) {
-      walk(pathNow)
-      strNow = strNow.slice(4)
-    }
-  })
+    const files = fs.readdirSync(dir);
+    strNow = tstr + strNow;
+    files.forEach((e) => {
+        const pathNow = path.join(dir, e);
+        console.log(strNow + path.relative(dir, pathNow));
+        const stats = fs.statSync(pathNow);
+        if (stats.isDirectory()) {
+            walk(pathNow);
+            strNow = strNow.slice(4);
+        }
+    });
 }
 
 walk(__dirname);
